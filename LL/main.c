ddc05e8b4a8d0ad5b162cebd3103c5078be84576
Sarthak Joshi <sarthakjoshi.in@gmail.com>
2019-04-12 21:35:53 +0530

    Initial Commit

---
+++ LL/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  LL
+//
+//  Created by Apple 1 on 12/04/19.
+//  Copyright © 2019 3B1B. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
